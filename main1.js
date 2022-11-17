4d63f70be500748a7b7895299d8214107e8e5302
nitish1899 <nkword1899@gmail.com>
2022-11-17 23:30:44 +0530

    CreatingNode&ModifyingDOM

--- main1.js
+++ main1.js
@@ -82,8 +82,9 @@
 // var submit=document.querySelector('input[type="submit"]');
 // submit.value='SEND';
 
-// var item=document.querySelector('.list-group-item');
-// item.style.color='red';
+var item=document.querySelector('.list-group-item');
+item.textContent='Nitish';
+item.style.color='red';
 
 // var lastitem=document.querySelector('.list-group-item:last-child');
 // lastitem.style.color='blue';
@@ -113,7 +114,7 @@
 
 //Transversing the DOM 
 
-var itemList=document.querySelector('#items');
+//var itemList=document.querySelector('#items');
 //praentNode
 // console.log(itemList.parentNode);
 // itemList.parentNode.style.backgroundColor='#f4f4f4';
@@ -158,76 +159,110 @@ var itemList=document.querySelector('#items');
 //CreateElement
 
 //Create a Div
-// var newDiv= document.createElement('div');
-// console.log(newDiv);
+var newDiv=document.createElement('div');
+var newLi=document.createElement('li');
 
-// //Add a class
-// newDiv.className='hello';
+//Add a class
+newDiv.className='hello';
+newLi.className='list-group-item';
 
-// //Add id
-// newDiv.id='hello1';
+//Add id
+newDiv.id='hello1';
 
-// //add Attr
-// newDiv.setAttribute('title','Hello Div');
+//add Attr
+newDiv.setAttribute('title','Hello Div');
 
-// //Create text node
-// var newDivText=Document.querySelector('header.container');
-// var h1=document.querySelector('header h1');
-// console.log(newDiv);
+//Create text node
+var  newDivText=document.createTextNode('HEllo word');
+var  newLiText=document.createTextNode('HEllo word');
 
-// newDiv.style.fontSize='30px';
-// CredentialsContainer.insertBefore(newDiv,h1);
+//Add text to div
+newDiv.appendChild(newDivText);
+newLi.appendChild(newLiText);
+
+var container=document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+newDiv.style.fontSize='30px';
+container.insertBefore(newDiv, h1);
+
+
+console.log(newDiv);
+console.log(newLi);
+
+var ul=document.getElementById('items');
+console.log(ul.innerHTML);
+ul.insertBefore(newLi,ul.children[0]);
+//ul.innerHTML=newLi+ul.innerHTML;
+console.log(ul.innerHTML);
 
 
 ///Adding a li element, delete button and removing li element
-var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
+// var form = document.getElementById('addForm');
+// var itemList = document.getElementById('items');
+// var filter = document.getElementById('filter');
+// // Form submit event
+// form.addEventListener('submit', addItem);
+// // Delete event
+// itemList.addEventListener('click', removeItem);
+// // Filter event
+// filter.addEventListener('keyup', filterItems);
 
-// Form submit event
-form.addEventListener('submit', addItem);
-// Delete event
-itemList.addEventListener('click', removeItem);
+// // Add item
+// function addItem(e){
+//   e.preventDefault();
 
+//   // Get input value
+//   var newItem = document.getElementById('item').value;
 
-// Add item
-function addItem(e){
-  e.preventDefault();
+//   // Create new li element
+//   var li = document.createElement('li');
+//   // Add class
+//   li.className = 'list-group-item';
+//   // Add text node with input value
+//   li.appendChild(document.createTextNode(newItem));
 
-  // Get input value
-  var newItem = document.getElementById('item').value;
+//   // Create del button element
+//   var deleteBtn = document.createElement('button');
 
-  // Create new li element
-  var li = document.createElement('li');
-  // Add class
-  li.className = 'list-group-item';
-  // Add text node with input value
-  li.appendChild(document.createTextNode(newItem));
+//   // Add classes to del button
+//   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
 
-  // Create del button element
-  var deleteBtn = document.createElement('button');
+//   // Append text node
+//   deleteBtn.appendChild(document.createTextNode('X'));
 
-  // Add classes to del button
-  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+//   // Append button to li
+//   li.appendChild(deleteBtn);
 
-  // Append text node
-  deleteBtn.appendChild(document.createTextNode('X'));
+//   // Append li to list
+//   itemList.appendChild(li);
+// }
 
-  // Append button to li
-  li.appendChild(deleteBtn);
+// // Remove item
+// function removeItem(e){
+//   if(e.target.classList.contains('delete')){
+//     if(confirm('Are You Sure?')){
+//       var li = e.target.parentElement;
+//       itemList.removeChild(li);
+//     }
+//   }
+// }
 
-  // Append li to list
-  itemList.appendChild(li);
-}
-
-// Remove item
-function removeItem(e){
-  if(e.target.classList.contains('delete')){
-    if(confirm('Are You Sure?')){
-      var li = e.target.parentElement;
-      itemList.removeChild(li);
-    }
-  }
-}
+// // Filter Items
+// function filterItems(e){
+//     // convert text to lowercase
+//     var text = e.target.value.toLowerCase();
+//     // Get lis
+//     var items = itemList.getElementsByTagName('li');
+//     // Convert to an array
+//     Array.from(items).forEach(function(item){
+//       var itemName = item.firstChild.textContent;
+//       if(itemName.toLowerCase().indexOf(text) != -1){
+//         item.style.display = 'block';
+//       } else {
+//         item.style.display = 'none';
+//       }
+//     });
+//   }
 
 
 
