bb6552feb049507601e4694ddac52985259e4506
nitish1899 <nkword1899@gmail.com>
2022-11-18 12:53:16 +0530

    appliedFilterProperty

--- main1.js
+++ main1.js
@@ -82,9 +82,9 @@
 // var submit=document.querySelector('input[type="submit"]');
 // submit.value='SEND';
 
-var item=document.querySelector('.list-group-item');
-item.textContent='Nitish';
-item.style.color='red';
+// var item=document.querySelector('.list-group-item');
+// item.textContent='Nitish';
+// item.style.color='red';
 
 // var lastitem=document.querySelector('.list-group-item:last-child');
 // lastitem.style.color='blue';
@@ -158,111 +158,123 @@ item.style.color='red';
 
 //CreateElement
 
-//Create a Div
-var newDiv=document.createElement('div');
-var newLi=document.createElement('li');
+// //Create a Div
+// var newDiv=document.createElement('div');
+// var newLi=document.createElement('li');
 
-//Add a class
-newDiv.className='hello';
-newLi.className='list-group-item';
+// //Add a class
+// newDiv.className='hello';
+// newLi.className='list-group-item';
 
-//Add id
-newDiv.id='hello1';
+// //Add id
+// newDiv.id='hello1';
 
-//add Attr
-newDiv.setAttribute('title','Hello Div');
+// //add Attr
+// newDiv.setAttribute('title','Hello Div');
 
-//Create text node
-var  newDivText=document.createTextNode('HEllo word');
-var  newLiText=document.createTextNode('HEllo word');
+// //Create text node
+// var  newDivText=document.createTextNode('HEllo word');
+// var  newLiText=document.createTextNode('HEllo:::::word');
 
-//Add text to div
-newDiv.appendChild(newDivText);
-newLi.appendChild(newLiText);
+// //Add text to div
+// newDiv.appendChild(newDivText);
+// newLi.appendChild(newLiText);
 
-var container=document.querySelector('header .container');
-var h1=document.querySelector('header h1');
-newDiv.style.fontSize='30px';
-container.insertBefore(newDiv, h1);
-
-
-console.log(newDiv);
-console.log(newLi);
-
-var ul=document.getElementById('items');
-console.log(ul.innerHTML);
-ul.insertBefore(newLi,ul.children[0]);
-//ul.innerHTML=newLi+ul.innerHTML;
-console.log(ul.innerHTML);
+// var container=document.querySelector('header .container');
+// var h1=document.querySelector('header h1');
+// newDiv.style.fontSize='30px';
+// container.insertBefore(newDiv, h1);
 
 
-///Adding a li element, delete button and removing li element
-// var form = document.getElementById('addForm');
-// var itemList = document.getElementById('items');
-// var filter = document.getElementById('filter');
-// // Form submit event
-// form.addEventListener('submit', addItem);
-// // Delete event
-// itemList.addEventListener('click', removeItem);
-// // Filter event
-// filter.addEventListener('keyup', filterItems);
+// console.log(newDiv);
+// console.log(newLi);
 
-// // Add item
-// function addItem(e){
-//   e.preventDefault();
+// var ul=document.getElementById('items');
+// console.log(ul.innerHTML);
+// ul.insertBefore(newLi,ul.children[0]);
+// //ul.innerHTML=newLi+ul.innerHTML;
+// console.log(ul.innerHTML);
 
-//   // Get input value
-//   var newItem = document.getElementById('item').value;
+//Adding a li and div element, delete and edit button and removing li element
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
 
-//   // Create new li element
-//   var li = document.createElement('li');
-//   // Add class
-//   li.className = 'list-group-item';
-//   // Add text node with input value
-//   li.appendChild(document.createTextNode(newItem));
+// Add item
+function addItem(e){
+  e.preventDefault();
 
-//   // Create del button element
-//   var deleteBtn = document.createElement('button');
+  // Get input value
+  var newItem = document.getElementById('item').value;
+  var newItem2 = document.getElementById('item-description').value;
+  var liItem=`${newItem} ${newItem2}`;
+  // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'list-group-item';
+  
+  // Add text node with input value
+  li.appendChild(document.createTextNode(liItem));
+  
+  // Create del button element
+  var deleteBtn = document.createElement('button');
+  var editBtn = document.createElement('button');
 
-//   // Add classes to del button
-//   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  // Add classes to del button
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
 
-//   // Append text node
-//   deleteBtn.appendChild(document.createTextNode('X'));
+//style="float:right;" type="submit" id="edit-button">Edit
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
+  editBtn.appendChild(document.createTextNode('Edit'));
 
-//   // Append button to li
-//   li.appendChild(deleteBtn);
+  //Add id
+  editBtn.id='edit-button';
+  //Add attribute
+  editBtn.setAttribute('title','submit');
+  editBtn.style="float:right;"
+  
+  // Append button to li
+  li.appendChild(deleteBtn);
+  li.appendChild(editBtn);
+  
+  // Append li to list
+  itemList.appendChild(li);
+  
+}
 
-//   // Append li to list
-//   itemList.appendChild(li);
-// }
+// Remove item
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
 
-// // Remove item
-// function removeItem(e){
-//   if(e.target.classList.contains('delete')){
-//     if(confirm('Are You Sure?')){
-//       var li = e.target.parentElement;
-//       itemList.removeChild(li);
-//     }
-//   }
-// }
-
-// // Filter Items
-// function filterItems(e){
-//     // convert text to lowercase
-//     var text = e.target.value.toLowerCase();
-//     // Get lis
-//     var items = itemList.getElementsByTagName('li');
-//     // Convert to an array
-//     Array.from(items).forEach(function(item){
-//       var itemName = item.firstChild.textContent;
-//       if(itemName.toLowerCase().indexOf(text) != -1){
-//         item.style.display = 'block';
-//       } else {
-//         item.style.display = 'none';
-//       }
-//     });
-//   }
+// Filter Items
+function filterItems(e){
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get lis
+    var items = itemList.getElementsByTagName('li');
+    // Convert to an array
+    Array.from(items).forEach(function(item){
+      var itemName = item.firstChild.textContent;
+      if(itemName.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
 
 
 
